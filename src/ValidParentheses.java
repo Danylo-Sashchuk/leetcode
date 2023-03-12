d644e0491871f8b1782f2bf9ca9dcff345b8155d
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-03-12 19:59:33 -0400

    init commit & valid parantheses

---
+++ src/ValidParentheses.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println("(");
+    }
+    public static boolean isValid(String s) {
+        HashMap<Character, Character> paran = new HashMap<>();
+        paran.put('(', ')');
+        paran.put('{', '}');
+        paran.put('[', ']');
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (paran.containsKey(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            } else if (paran.containsValue(s.charAt(i))) {
+                if (paran.get(stack.pop()) != s.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
