b0fadfe171d3b54cd69bfdd975ee6be6589c3d6f
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-03-12 21:36:42 -0400

    MergeSortedArray implemented

---
+++ src/arrays/MergeSortedArray.java
@@ -0,0 +1,37 @@
+package arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        print(merge(new int [] {1,2,3,0,0,0}, 3, new int [] {2,5,6}, 3));
+        print(merge(new int [] {1}, 1, new int [] {}, 0));
+        print(merge(new int [] {0}, 0, new int [] {1}, 1));
+    }
+
+    public static void print(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();;
+    }
+
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] res = new int[n + m];
+        int firstPointer = 0;
+        int secondPointer = 0;
+        int resPointer = 0;
+        while (firstPointer < m && secondPointer < n) {
+            if (nums1[firstPointer] <= nums2[secondPointer]) {
+                res[resPointer++] = nums1[firstPointer++];
+            } else {
+                res[resPointer++] = nums2[secondPointer++];
+            }
+        }
+        while (firstPointer < m) {
+            res[resPointer++] = nums1[firstPointer++];
+        }
+        while (secondPointer < n) {
+            res[resPointer++] = nums2[secondPointer++];
+        }
+        return res;
+    }
+}
