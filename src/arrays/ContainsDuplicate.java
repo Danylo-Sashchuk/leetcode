af65bc8cfe6f118533f3fe23e4718fcd1ca1d342
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-05-05 19:40:46 -0400

    completed ContainsDuplicate

---
+++ src/arrays/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
