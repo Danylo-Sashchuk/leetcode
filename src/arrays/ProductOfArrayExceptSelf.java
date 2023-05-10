fafd707be4ff9bd8e4f28c7b3ebcc8627ac49834
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-05-09 20:14:06 -0400

    prefix and postfix array

--- src/arrays/ProductOfArrayExceptSelf.java
+++ src/arrays/ProductOfArrayExceptSelf.java
@@ -12,17 +12,13 @@ public class ProductOfArrayExceptSelf {
         int[] answer = new int[nums.length];
         int[] left = new int[nums.length];
         int[] right = new int[nums.length];
-        int product = 1;
         left[0] = 1;
+        right[nums.length - 1] = 1;
         for (int i = 1; i < nums.length; i++) {
-            product *= nums[i - 1];
-            left[i] = product;
+            left[i] = nums[i - 1] * left[i - 1];
         }
-        product = 1;
-        right[nums.length-1] = 1;
         for (int i = nums.length - 2; i >= 0; i--){
-            product *= nums[i + 1];
-            right[i] = product;
+            right[i] = nums[i + 1] * right[i + 1];
         }
         System.out.println(Arrays.toString(left));
         System.out.println(Arrays.toString(right));
