f8e34dc85b4eb5828edebe73775bd9849bdc74f7
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-05-17 23:30:17 -0400

    third maximum number

---
+++ src/doublepointers/ThirdMaximumNumber.java
@@ -0,0 +1,28 @@
+package doublepointers;
+
+public class ThirdMaximumNumber {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 5, 3, 5};
+        System.out.println(thirdMax(nums));
+    }
+
+    public static int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE;
+        long second = first;
+        long third = first;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > first) {
+                third = second;
+                second = first;
+                first = nums[i];
+            } else if (nums[i] > second && nums[i] != first) {
+                third = second;
+                second = nums[i];
+            } else if (nums[i] > third && nums[i] != second && nums[i] != first) {
+                third = nums[i];
+            }
+        }
+        if (third == Long.MIN_VALUE) return (int) first;
+        return (int) third;
+    }
+}
