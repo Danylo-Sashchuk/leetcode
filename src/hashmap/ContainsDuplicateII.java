a6b1fce8d40ac46fe668ed1062a760618ec6195a
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-05-26 12:33:09 -0400

    ContainsDuplicateII

---
+++ src/hashmap/ContainsDuplicateII.java
@@ -0,0 +1,30 @@
+package hashmap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ContainsDuplicateII {
+    public static void main(String[] args) {
+        System.out.println(containsNearbyDuplicate(new int[]{1, 2, 3, 1}, 3));
+        System.out.println(containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1));
+        System.out.println(containsNearbyDuplicate(new int[]{1, 2, 3, 1, 2, 3}, 2));
+    }
+
+    static public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                List<Integer> list = map.get(nums[i]);
+                for (int j : list) {
+                    if (Math.abs(j - i) <= k) return true;
+                }
+                list.add(i);
+            } else {
+                map.put(nums[i], new ArrayList<>(List.of(i)));
+            }
+        }
+        return false;
+    }
+}
