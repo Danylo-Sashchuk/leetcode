bda67c4a0d370c562a2476e77783299a8898b2ca
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-20 20:55:15 -0400

    Added Delete Duplicate

---
+++ src/linkedlist/removeDuplicates.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+        ListNode node = deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2))));
+        ListNode node2 = deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3))))));
+
+        System.out.println();
+    }
+
+    static public ListNode deleteDuplicates(ListNode head) {
+            if (head == null || head.next == null)
+                return head;
+            ListNode previous = head;
+            ListNode following = head.next;
+
+            while (following != null) {
+                if (following.val == previous.val) {
+                    previous.next = following.next;
+                } else {
+                    previous = previous.next;
+                }
+                following = following.next;
+            }
+            return head;
+    }
+}
