14c4a485fdc18b754a2681a7ef35ddaadbc4f949
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-21 21:29:13 -0400

    missing number through sum

--- src/arrays/MissingNumber.java
+++ src/arrays/MissingNumber.java
@@ -19,4 +19,14 @@ public class MissingNumber {
         }
         return -1;
     }
+
+    static public int missingNumber1(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += i;
+            sum -= nums[i];
+        }
+        sum += nums.length;
+        return sum;
+    }
 }
