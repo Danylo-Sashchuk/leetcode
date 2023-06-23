62639c393f928bd5ba5adb4c97c582adf2a49440
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-22 21:59:43 -0400

    min stack using nodes

---
+++ src/model/MinStack.java
@@ -0,0 +1,50 @@
+package model;
+
+public class MinStack {
+
+    private Node head;
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-1);
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.getMin());
+    }
+
+    public void push(int x) {
+        if (head == null) head = new Node(x, x, null);
+        else head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
