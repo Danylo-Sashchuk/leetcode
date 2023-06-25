eb8682358c5250fb92c58c2d182d931cff48d1b2
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-24 21:22:33 -0400

    daily temperature inizial

---
+++ src/stack/DailyTemperatures.java
@@ -0,0 +1,19 @@
+package stack;
+
+import java.util.Arrays;
+
+public class DailyTemperatures {
+    public static int[] dailyTemperatures(int[] temperatures) {
+        for (int i = 0; i < temperatures.length; i++) {
+
+        }
+        return temperatures;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+       // temperatures = new int[]{30, 40, 50, 60};
+        //temperatures = new int[]{30, 60, 90};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+    }
+}
