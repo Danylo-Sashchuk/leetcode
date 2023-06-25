c9ebacafd23de8a1b7e41ccd741fc8de3b2b880f
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-06-25 00:01:40 -0400

    baseball game using deque arraydeque

--- src/stack/BaseballGame.java
+++ src/stack/BaseballGame.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Stack;
 
 public class BaseballGame {
@@ -9,28 +11,53 @@ public class BaseballGame {
         System.out.println(calPoints(new String[]{"1", "C"}));
 
     }
-
     static public int calPoints(String[] operations) {
-        Stack<String> record = new Stack<>();
+        Deque<Integer> record = new ArrayDeque<>();
+        int sum = 0;
         for (int i = 0; i < operations.length; i++) {
             switch (operations[i]) {
                 case "+" -> {
-                    int a = Integer.parseInt(record.pop());
-                    int b = Integer.parseInt(record.pop());
-                    int c = a + b;
-                    record.push(String.valueOf(b));
-                    record.push(String.valueOf(a));
-                    record.push(String.valueOf(c));
+                    int a = record.pop();
+                    int b = a + record.peek();
+                    record.push(a);
+                    record.push(b);
+                    sum += b;
                 }
                 case "D" -> {
-                    int a = Integer.parseInt(record.peek());
-                    int b = a * 2;
-                    record.push(String.valueOf(b));
+                    sum += record.peek() * 2;
+                    record.push(record.peek() * 2);
                 }
-                case "C" -> record.pop();
-                default -> record.push(operations[i]);
+                case "C" -> {
+                    sum -= record.peek();
+                    record.pop();
+                }
+                default -> record.push(Integer.valueOf(operations[i]));
             }
         }
-        return record.stream().map(Integer::parseInt).reduce(0, Integer::sum);
+        return sum;
     }
+
+//    static public int calPoints(String[] operations) {
+//        Stack<String> record = new Stack<>();
+//        for (int i = 0; i < operations.length; i++) {
+//            switch (operations[i]) {
+//                case "+" -> {
+//                    int a = Integer.parseInt(record.pop());
+//                    int b = Integer.parseInt(record.pop());
+//                    int c = a + b;
+//                    record.push(String.valueOf(b));
+//                    record.push(String.valueOf(a));
+//                    record.push(String.valueOf(c));
+//                }
+//                case "D" -> {
+//                    int a = Integer.parseInt(record.peek());
+//                    int b = a * 2;
+//                    record.push(String.valueOf(b));
+//                }
+//                case "C" -> record.pop();
+//                default -> record.push(operations[i]);
+//            }
+//        }
+//        return record.stream().map(Integer::parseInt).reduce(0, Integer::sum);
+//    }
 }
