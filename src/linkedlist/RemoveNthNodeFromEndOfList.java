629355a8b0a6879966c66806a03962eaf73c2cc2
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-07-28 22:07:54 -0400

    Readability improvements

--- src/linkedlist/RemoveNthNodeFromEndOfList.java
+++ src/linkedlist/RemoveNthNodeFromEndOfList.java
@@ -3,7 +3,7 @@ package linkedlist;
 public class RemoveNthNodeFromEndOfList {
     public static void main(String[] args) {
         //ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
-//        removeNthFromEnd(head, 2);
+        //        removeNthFromEnd(head, 2);
         ListNode head = new ListNode(1);
         removeNthFromEnd(head, 1);
     }
@@ -11,8 +11,10 @@ public class RemoveNthNodeFromEndOfList {
     static public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode faster = head;
         ListNode slower = head;
-        for (int i = 0; i < n; i++) faster = faster.next;
-        if (faster == null) return head.next;
+        for (int i = 0; i < n; i++)
+            faster = faster.next;
+        if (faster == null)
+            return head.next;
         while (faster.next != null) {
             faster = faster.next;
             slower = slower.next;
